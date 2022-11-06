5cf80a8cfbedb56f337c4d28c7467f91b0cc165d
Barry <snine192@gmail.com>
2022-11-07 00:05:57 +0800

    20221107

--- VUE/js/App.js
+++ VUE/js/App.js
@@ -1,12 +1,16 @@
-import Nav from './components/Nav.js';
+import Nav from './components/Nav.js' 
+import Div from './components/Nav.js';
 export default {
     data(){
         return {
             title:'Hello Vue',
             qtv: 223,
+            角色 : ['日', '一', '二', '三', '四', '五', '六']
         }
     },
     components:{
-        'app-nav':Nav
+        'app-nav':Nav,
+        'app-div':Div,
+         
     }
 }
---
+++ VUE/js/components/Div.js
@@ -0,0 +1,10 @@
+export default {
+    template:`
+    <div>
+        <a href="#">About</a>
+        <a href="#">Works</a>
+        <a href="#">Service</a>
+        <a href="#">Contact</a>
+    </div>
+    `
+}
